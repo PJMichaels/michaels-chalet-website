8a6c23afdfcd4ebc11e9a44240f71cfd61eed13d
Phil Michaels <93264586+PJMichaels@users.noreply.github.com>
2023-10-30 00:00:09 -0400

    seems to do the same thing

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,10 +126,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# I think this a bad version based on my post not working
-# CORS_ORIGIN_WHITELIST = [
-#      'http://localhost:3000'
-# ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # If your React app runs on this port
