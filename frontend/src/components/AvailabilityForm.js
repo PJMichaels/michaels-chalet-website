a39b1df43323c8811d55d2130c970e2a590e41ec
Phil Michaels <93264586+PJMichaels@users.noreply.github.com>
2023-11-23 08:48:33 -0500

    add availability page

---
+++ frontend/src/components/AvailabilityForm.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Axios from 'axios';
+// import './BookingForm.css'
+
+const AvailabilityForm = ({start_date, end_date}) => {
+    const [formData, setFormData] = useState({
+        startDate: start_date.toDateString(),
+        endDate: end_date.toDateString(),
+        reason: "",
+    });
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    function formatDate(date) {
+        const d = new Date(date);
+        let month = '' + (d.getMonth() + 1);
+        let day = '' + d.getDate();
+        const year = d.getFullYear();
+    
+        if (month.length < 2) 
+            month = '0' + month;
+        if (day.length < 2) 
+            day = '0' + day;
+    
+        return [year, month, day].join('-');
+    }
+
+const navigate = useNavigate();
+
+  const redirectToPage = () => {
+    navigate('/booking/');}
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Post booking request to Django backend
+        Axios.post('/api/available/', {
+            "start_date": formatDate(start_date),
+            "end_date": formatDate(end_date),
+            "reason": formData.reason
+        })
+            .then((response) => {
+                console.log(response.data);
+                redirectToPage();
+            })
+            .catch((error) => {
+                // This error should really be in a modal long term
+                console.error("An error occurred while posting data: ", error);
+            });
+    };
+
+    return (
+        <div className="booking-form">
+         <form onSubmit={handleSubmit}>
+                 <label>Reason For Availability:</label>
+                 <textarea name="reason" onChange={handleChange} required></textarea>
+             <button className="button" type="submit">Provision Dates</button>
+         </form>
+     </div>
+    );
+};
+
+export default AvailabilityForm;
---
+++ frontend/src/pages/AvailabilityPage.js
@@ -0,0 +1,119 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import 'react-calendar/dist/Calendar.css';
+import AvailabilityForm from "../components/AvailabilityForm";
+import AdminCalendar from "../components/AdminCalendar";
+import './AvailabilityPage.css';
+
+const AvailabilityPage = () => {
+  // variables for available dates to be stored or error states
+    // from available data, then assign values to those variables
+    const [provisionedAPI, setProvisionedAPI] = useState([]);
+    const [provisionedAPIError, setProvisionedAPIError] = useState(null);
+
+    // Queries available api and populate variables
+    useEffect(() => {
+        // Assume your API endpoint is 'http://yourapi.com/bookings'
+        axios.get('/api/available/')
+            .then((response) => {
+                setProvisionedAPI(response.data);
+            })
+            .catch((err) => {
+                setProvisionedAPIError(err.toString());
+            });
+    }, []); // Empty dependency array means this useEffect runs once when component mounts
+
+    // const allowedRanges = allowedData.map((entry, index) => 
+    // ({'start': new Date(entry.start_date), 'end': new Date(entry.end_date)})
+    // );
+
+
+    // variables for booking data to be stored or error states
+    // from booking data, then assign values to those variables
+    const [bookedAPI, setBookedAPI] = useState([]);
+    const [bookedAPIError, setBookedAPIError] = useState(null);
+
+    // this code actually populates variables
+    useEffect(() => {
+        // Assume your API endpoint is 'http://yourapi.com/bookings'
+        axios.get('/api/bookings/')
+            .then((response) => {
+                setBookedAPI(response.data);
+            })
+            .catch((err) => {
+                setBookedAPIError(err.toString());
+            });
+    }, []); // Empty dependency array means this useEffect runs once when component mounts
+
+    // starts the calendar creation process
+    const getAvailableDates = (provisionedRanges, bookedRanges) => {
+        /*
+        Function takes in two arrays of objects, where each object must
+        contain a "start_date" and "end_date" key and date string value.
+        */
+        const expandDateRanges = (dates) => {
+            let result = [];
+        
+            dates.forEach(range => {
+                let current = new Date(range.start_date);
+                let end = new Date(range.end_date);
+        
+                while (current <= end) {
+                    // push after date change to account zero index of day in calendar
+                    current.setDate(current.getDate() + 1);
+                    result.push(current.toDateString());
+                }
+            });
+            return result;
+        };
+    
+        const bookedDates = expandDateRanges(bookedRanges);
+        const provisionedDates = expandDateRanges(provisionedRanges);
+        const availableDates = [];
+    
+        provisionedDates.forEach((date) => {
+            if (!bookedDates.includes(date)) {
+                availableDates.push(date);
+            }
+        })
+        return availableDates;
+    }
+
+    const availableDates = getAvailableDates(provisionedAPI, bookedAPI);
+
+    const [selectedDates, setDates] = React.useState([new Date(), new Date()]);
+
+
+    const handleDateChange = newDates => {
+        setDates(newDates);
+    };
+
+    // Actively working on this to add delete available provisioned to admin page
+    // const deleteProvisioned = (id) => {
+    //     axios.delete(`/api/available/${id}/`)
+    //         .then(() => {
+    //             // Remove the deleted booking from the state
+    //             const updatedBookings = availableDates.filter(provisioned => provisioned.id !== id);
+    //             setBookingData(updatedBookings);
+    //         })
+    //         .catch(err => {
+    //             setBookedAPIError(err.toString());
+    //         });
+    // };
+
+  return (
+    <div className="booking-container">
+        <AvailabilityForm
+             start_date={selectedDates[0]}
+             end_date= {selectedDates[1]}
+         />
+         <AdminCalendar 
+             availableDates={availableDates}
+             selectedDates={selectedDates}
+             handleDateChange = {handleDateChange}
+             />
+   </div>
+  );
+};
+
+export default AvailabilityPage;
