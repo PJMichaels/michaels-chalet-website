1bd73f7bd99e5392d2c8246b611ff3b0193cfe0e
Phil Michaels <93264586+PJMichaels@users.noreply.github.com>
2023-11-26 11:55:40 -0500

    remove artifacts

--- rebuild-start.ps1
+++
@@ -1,8 +0,0 @@
-# This is a one command approach to start this app from the repo root
-
-# Populate an updated requirements.txt file
-pipenv requirements > ./backend/requirements.txt
-
-# Build and run Docker Compose
-docker-compose build
-docker-compose up
--- start-app.ps1
+++
@@ -1,7 +0,0 @@
-# This is a one command approach to start this app from the repo root
-
-# Step 1 Populate an updated requirements.txt file
-pipenv requirements > ./backend/requirements.txt
-
-# Step 2 Run Docker Compose
-docker-compose up
