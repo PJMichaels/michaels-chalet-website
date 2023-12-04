531f9eca2a96734eb365db169c175874b26e9cd3
Phil Michaels <93264586+PJMichaels@users.noreply.github.com>
2023-12-03 22:27:54 -0500

    adding galley for image display w placeholder

--- frontend/src/pages/GalleryPage.js
+++ frontend/src/pages/GalleryPage.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import ImageGallery from 'react-image-gallery';
+import { images } from '../data/gallery-images';
 
 const GalleryPage = () => {
   return (
-    <div>
+    <div className='content-container'>
       <h1>Welcome to the Photo Gallery!</h1>
       <p>
         Place holder for where we can add our favorite
         photos of the house and surrounding area etc..
       </p>
+      <div className="image-gallery-wrapper">
+        <ImageGallery items={images} />      
+        </div>
     </div>
   );
 };
