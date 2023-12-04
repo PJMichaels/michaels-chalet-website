4abb52337ec4e9530db06ff657292e9aa2434bda
Phil Michaels <93264586+PJMichaels@users.noreply.github.com>
2023-12-03 22:27:00 -0500

    placeholder images

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import ImageSlider from '../components/ImageSlider';
+import './HomePage.css';
 
 const HomePage = () => {
+
+
+
   return (
     <div>
-      <h1>Hello, this is the Home Page!</h1>
+      {/* <h1>Hello, this is the Home Page!</h1> */}
+      <div className="content-container">
+        <ImageSlider />
+      </div>
+      
     </div>
   );
 };
