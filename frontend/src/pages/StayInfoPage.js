812f8ce66508b6c9ced9990754119ba9bd09b0d7
Phil Michaels <93264586+PJMichaels@users.noreply.github.com>
2023-12-21 10:49:23 -0500

    add stay info page

---
+++ frontend/src/pages/StayInfoPage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const StayInfoPage = () => {
+  return (
+    <div className='content-container'>
+      <h1>Stay Information</h1>
+      <p>
+        Place to put details on the house as well as recommend hikes, site seeing, etc.
+      </p>
+      <ul>
+        <li>Thermostat Settings</li>
+        <li>Wifi Info</li>
+        <li>Pellet Stove Info</li>
+        <li>Coffee Maker Instructions</li>
+        <li>Using the public beach</li>
+        <li>Recommended Hikes</li>
+        <li>Favorite Restaurants</li>
+        <li>Grilling...</li>
+        <li>Fire Pit</li>
+        <li>Sauna - Coming Soon</li>
+      </ul>
+    </div>
+  );
+};
+
+export default StayInfoPage;
