442f1890bb2ed6f9165089efefc42c2f6c9205b8
Phil Michaels <93264586+PJMichaels@users.noreply.github.com>
2023-12-27 10:05:44 -0500

    added and organized house photos

--- frontend/src/components/ImageSlider.js
+++ frontend/src/components/ImageSlider.js
@@ -6,10 +6,26 @@ class ImageSlider extends Component {
 
   state = { currentImageIndex: 0 };
 
-  images = ['../../ChaletLivingRoom.jpg', '../../ChaletKitchen.jpg'];
+  images = [
+    // '../../LivingRoom1.jpg',
+    '../../Photos/KitchenTable1.jpg',
+    '../../Photos/LivingRoom1-xmas.jpg',
+    '../../Photos/MainFloor1-xmas.jpg',
+    '../../Photos/MainFloor2-xmas.jpg',
+    '../../Photos/Kitchen1.jpg',
+    '../../Photos/MainFloorBath1.jpg',
+    '../../Photos/SecondBedroom1.jpg',
+    '../../Photos/SecondBedroom2.jpg',
+    '../../Photos/FamilyRoom1.jpg',
+    '../../Photos/FamilyRoom2.jpg',
+    '../../Photos/DownStairsBath1.jpg',
+    '../../Photos/Office1.jpg',
+    '../../Photos/Office2.jpg',
+    '../../Photos/Office3.jpg'
+  ];
 
   componentDidMount = () => {
-    this.interval = setInterval(this.nextImage, 10000); // change image every 3 seconds
+    this.interval = setInterval(this.nextImage, 5000); // change image every 3 seconds
   }
 
   componentWillUnmount = () => {
