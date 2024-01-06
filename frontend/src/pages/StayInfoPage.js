1bd664416666b5df7bff0471ecb12e5926ba357f
Phil Michaels <93264586+PJMichaels@users.noreply.github.com>
2024-01-06 17:19:02 -0500

    created template info page

--- frontend/src/pages/StayInfoPage.js
+++ frontend/src/pages/StayInfoPage.js
@@ -6,10 +6,16 @@ const StayInfoPage = () => {
       <h1>Stay Information</h1>
       <p>
         Place to put details on the house as well as recommend hikes, site seeing, etc.
+
+        <br></br>
+        <br></br>Address: 37 C Street Conway, NH 03818
+        <br></br>Placeholder for Door entry pw
+        <br></br>Warning about needing 4WD or AWD in Winter
       </p>
       <ul>
         <li>Thermostat Settings</li>
         <li>Wifi Info</li>
+        <li>Door Lock Info</li>
         <li>Pellet Stove Info</li>
         <li>Water Main..</li> 
         <li>Coffee Maker Instructions</li>
