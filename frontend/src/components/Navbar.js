6c56c3d108f0b03340f6c83bafa97a44b473de14
Phil Michaels <93264586+PJMichaels@users.noreply.github.com>
2024-01-06 17:18:39 -0500

    added auth only navbar links

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -11,19 +11,23 @@ const Navbar = () => {
         {/* ... Public Links ... */}
         <li><Link to="/">Home</Link></li>
         <li><Link to="/booking">Booking</Link></li>
-        <li><Link to="/reservations">Reservations</Link></li>
-        <li><Link to="/gallery">Photo Gallery</Link></li>
+        {/* <li><Link to="/gallery">Photo Gallery</Link></li> */}
         <li><Link to="/saco-river">Saco River Tubing</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/about">About</Link></li>
+        {/* <li><Link to="/contact">Contact</Link></li> */}
+        {/* <li><Link to="/about">About</Link></li> */}
         {/* Conditional link for Login/Logout */}
         {!isLoggedIn ? (
           <li><Link to="/login">Login</Link></li>
         ) : (
+          <div>
+          <li><Link to="/reservations">Reservations</Link></li>
+          <li><Link to="/stayinfo">Stay Info</Link></li>
+          <li><Link to="/checkout">Checkout Info</Link></li>
           <li>
             {/* Use the logout function directly */}
             <button onClick={logout}>Logout</button>
           </li>
+          </div>
         )}
       </ul>
     </nav>
