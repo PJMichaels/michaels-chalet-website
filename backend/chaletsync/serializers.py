f58b1652bef1f9e629f97ea606f33409d9d44d32
Phil Michaels <93264586+PJMichaels@users.noreply.github.com>
2024-01-19 23:55:20 -0500

    added user model and rbac check to views

--- backend/chaletsync/serializers.py
+++ backend/chaletsync/serializers.py
@@ -1,5 +1,11 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Availability, Bookings
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'groups']
 
 class AvailabilitySerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +17,5 @@ class BookingsSerializer(serializers.ModelSerializer):
         model = Bookings
         fields = ['id', 'name', 'group_size', 'start_date', 'end_date', 'note']
 
+
+# create a serializer for availability just for guests
--- backend/chaletsync/views.py
+++ backend/chaletsync/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import BookingsSerializer, AvailabilitySerializer
+from django.contrib.auth.models import Group, User
+from .serializers import UserSerializer, BookingsSerializer, AvailabilitySerializer #, GroupSerializer
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsAdminUser, IsGuestUser, IsLimitedGuestUser
 from .models import Bookings, Availability
 
 # Create your views here.
 
+# view for admin add/update/delete users - not sure about passwords
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated, IsAdminUser)
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+# view for admin to add/update/delete all bookings
 class BookingsView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated, IsGuestUser)
     serializer_class = BookingsSerializer
     queryset = Bookings.objects.all()
 
+# view for admin to add/update/delete all availability
 class AvailabilityView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = AvailabilitySerializer
     queryset = Availability.objects.all()
+
+# add multiple views for admin to see all other models ...
+
+# create a view for only availability dates and nothing else
+        
+# create a view or serializer for bookings only for the the current user
+
+# eventually add views for float trips, guest book, etc..
