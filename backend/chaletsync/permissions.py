b9ccfc01271e52ab8c429b9ca6daf6ebeb943b4f
Phil Michaels <93264586+PJMichaels@users.noreply.github.com>
2024-01-19 23:53:30 -0500

    add permissions for role base auth

---
+++ backend/chaletsync/permissions.py
@@ -0,0 +1,22 @@
+from rest_framework import permissions
+
+class IsAdminUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.groups.filter(name='Admin').exists()
+    
+class IsGuestUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.groups.filter(name='Guest').exists()
+    
+class IsLimitedGuestUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.groups.filter(name='LimitedGuest').exists()
+    
+# Experimental class to check multiple roles AND Authentication!
+# class IsAuthenticatedAndAdminOrGuest(permissions.BasePermission):
+#     def has_permission(self, request, view):
+#         is_authenticated = request.user and request.user.is_authenticated
+#         is_admin = request.user.groups.filter(name='Admin').exists()
+#         is_guest = request.user.groups.filter(name='Guest').exists()
+
+#         return is_authenticated and (is_admin or is_guest)
