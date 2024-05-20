bafac0f1d5d9cb3eb4c2d0464bba70217c047a7b
Phil Michaels <93264586+PJMichaels@users.noreply.github.com>
2024-05-19 23:25:16 -0400

    create gallery page

--- frontend/src/pages/GalleryPage.js
+++ frontend/src/pages/GalleryPage.js
@@ -1,21 +1,18 @@
 import React from 'react';
 // import ImageGallery from 'react-image-gallery';
 // import { images } from '../data/gallery-images';
-import './GalleryPage.css';
+import ImageSlider from '../components/ImageSlider';
+// import './GalleryPage.css';
 
 const GalleryPage = () => {
   return (
-    <div></div>
-    // <div className='content-container'>
-    //   <h1>Welcome to the Photo Gallery!</h1>
-    //   <p>
-    //     Place holder for where we can add our favorite
-    //     photos of the house and surrounding area etc..
-    //   </p>
-    //   <div className="image-gallery-wrapper">
-    //     <ImageGallery items={images} />      
-    //     </div>
-    // </div>
+    <div>
+      {/* <h1>Hello, this is the Home Page!</h1> */}
+      <div className="content-container">
+        <ImageSlider />
+      </div>
+      
+    </div>
   );
 };
 
