50382481a784562ca6084a36b1acfe7c6420e7c0
Phil Michaels <93264586+PJMichaels@users.noreply.github.com>
2024-05-19 23:22:13 -0400

    add placeholder

--- frontend/src/components/RequestForm.js
+++ frontend/src/components/RequestForm.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Axios from 'axios';
+// import { useAuth } from './AuthContext';
 // import './BookingForm.css'
 
 
