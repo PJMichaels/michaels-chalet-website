3f97280f442b22a33478d412163d3be7401152b8
Phil Michaels <93264586+PJMichaels@users.noreply.github.com>
2024-05-23 00:34:09 -0400

    update export

--- frontend/src/funcs/calendarFuncs.js
+++ frontend/src/funcs/calendarFuncs.js
@@ -5,10 +5,15 @@ const dateRangeToArray = (first_day, last_day) => {
     var current_day = new Date(first_day);
     var end_day = new Date(last_day);
 
+    // // test adding one date to offset calendar zero index
+    // current_day = current_day.getDate() + 1;
+    // end_day = end_day.getDate() + 1;
+
     // Iterate through date and add to array through end date
     while (current_day <= end_day) {
-        date_array.push(current_day.toDateString());
+        // push after date change to account zero index of day in calendar
         current_day.setDate(current_day.getDate() + 1);
+        date_array.push(current_day.toDateString());
     }
     return date_array;
 };
@@ -40,7 +45,7 @@ const getAvailableDates = (provisionedRanges, bookedRanges, minDaysOut) => {
 
     // populate areadyBookedDates array with all date strings converted from ranges
     bookedRanges.forEach(booking => {
-        console.log(booking.arrival_date, booking.departure_date);
+        // console.log(booking.arrival_date, booking.departure_date);
         bookedDates = bookedDates.concat(dateRangeToArray(booking.arrival_date, booking.departure_date));
     })
     
@@ -64,25 +69,29 @@ const getAvailableDates = (provisionedRanges, bookedRanges, minDaysOut) => {
 }
 
 
+const getProvisionedDates = (provisionedRanges, bookedRanges) => {
+    
+    // Set variables
+    var bookedDates = [];
+    var provisionedDates = [];
+    
+    // check that bookedRanges is not empty
 
-// const provisionedAPI = [
-//     {
-//         "id": 1,
-//         "reason": "test",
-//         "start_date": "2024-05-11",
-//         "end_date": "2024-05-13"
-//     },
-//     {
-//         "id": 2,
-//         "reason": "test",
-//         "start_date": "2024-05-20",
-//         "end_date": "2024-05-22"
-//     }
-// ];
+    // populate areadyBookedDates array with all date strings converted from ranges
+    bookedRanges.forEach(booking => {
+        // console.log(booking.arrival_date, booking.departure_date);
+        bookedDates = bookedDates.concat(dateRangeToArray(booking.arrival_date, booking.departure_date));
+    })
+    
 
-// const bookedAPI = [];
+    // check that provisionedRanges is not empty
 
-// console.log(getDates(provisionedAPI, bookedAPI));
+    provisionedRanges.forEach(provisioned => {
+        provisionedDates = provisionedDates.concat(dateRangeToArray(provisioned.start_date, provisioned.end_date));
+    })
 
+    return bookedDates, provisionedDates;
+        
+}
 
-export default getAvailableDates;
+export {getAvailableDates, dateRangeToArray};
