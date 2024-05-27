87fb1327dc97983f409863a9eba1900c20965306
Phil Michaels <93264586+PJMichaels@users.noreply.github.com>
2024-05-27 12:58:59 -0400

    start migrating api calls to one loc

--- frontend/src/utilities/api_funcs.js
+++ frontend/src/utilities/api_funcs.js
@@ -1,33 +1,59 @@
 // api_funcs.js
+import axios from 'axios';
 
-const API_ENDPOINT = 'http://yourapi.com/api/users';
 
-export const fetchUsers = async () => {
-    return fetch(API_ENDPOINT).then(res => res.json());
+// Base URL for your API
+const BASE_URL = '/api/';
+
+
+// Function to fetch data from request endpoint
+export const fetchRequestsData = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/requests/`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
-export const createUser = async (userData) => {
-    return fetch(API_ENDPOINT, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userData),
-    });
-};
 
-export const updateUser = async (userId, userData) => {
-    return fetch(`${API_ENDPOINT}/${userId}/`, {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userData),
-    });
-};
+// Function to fetch data from booking endpoint
+export const fetchBookingsData = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/bookings/`);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
 
-export const deleteUser = async (userId) => {
-    return fetch(`${API_ENDPOINT}/${userId}/`, {
-        method: 'DELETE',
-    });
-};
+
+// export const fetchUsers = async () => {
+//     return fetch(API_ENDPOINT).then(res => res.json());
+// };
+
+// export const createUser = async (userData) => {
+//     return fetch(API_ENDPOINT, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(userData),
+//     });
+// };
+
+// export const updateUser = async (userId, userData) => {
+//     return fetch(`${API_ENDPOINT}/${userId}/`, {
+//         method: 'PATCH',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(userData),
+//     });
+// };
+
+// export const deleteUser = async (userId) => {
+//     return fetch(`${API_ENDPOINT}/${userId}/`, {
+//         method: 'DELETE',
+//     });
+// };
