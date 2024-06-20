b5214a299afc94c1b4a23cb3882392eba058eea1
Phil Michaels <93264586+PJMichaels@users.noreply.github.com>
2024-06-20 02:04:32 -0400

    extend token lifetime

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -169,7 +169,7 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_CREDENTIALS = True
 
 SIMPLE_JWT = {
-     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
      'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
      'ROTATE_REFRESH_TOKENS': True,
      'BLACKLIST_AFTER_ROTATION': True,
