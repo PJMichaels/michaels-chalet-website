20f69c56ba17f16929c53a72d15cebc23482279a
Phil Michaels <93264586+PJMichaels@users.noreply.github.com>
2024-06-20 02:24:36 -0400

    styling and cleanup

--- frontend/src/components/EditBookingForm.js
+++ frontend/src/components/EditBookingForm.js
@@ -1,16 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import Axios from 'axios';
-import { useAuth } from '../context/AuthContext';
-// import './BookingForm.css'
 
 const EditBookingForm = ({requestObject, closeModal}) => {
-
-    // test to differentiate booking vs request
-    // if (requestObject.includes('status')) {
-
-    // }
-    const {isLoading, username } = useAuth();
 
     const [formData, setFormData] = useState({
         id: requestObject.id,
@@ -26,8 +17,8 @@ const EditBookingForm = ({requestObject, closeModal}) => {
     useEffect(() => {
         setFormData(formData => ({
             ...formData,
-            arrivalDate: new Date(requestObject.arrival_date), //.toISOString().split('T')[0],
-            departureDate: new Date(requestObject.departure_date) //.toISOString().split('T')[0],
+            arrivalDate: new Date(requestObject.arrival_date),
+            departureDate: new Date(requestObject.departure_date)
         }));
     }, [requestObject.arrival_date, requestObject.departure_date]); // Update state when props change
 
@@ -53,14 +44,9 @@ const EditBookingForm = ({requestObject, closeModal}) => {
         return [year, month, day].join('-');
     }
 
-// const navigate = useNavigate();
-
-//   const redirectToPage = () => {
-//     navigate('/reservation-management');}
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // console.log("Placeholder for API call")
         // Post booking request to Django backend
         Axios.patch(`/api/bookings/${formData.id}/`, {
             "group_size": formData.groupSize,
@@ -83,33 +69,56 @@ const EditBookingForm = ({requestObject, closeModal}) => {
     };
 
     return (
-        <div>
-         <form onSubmit={handleSubmit}>
-            <label>Editing Booking ID: {requestObject.id}</label>
-            <br></br>
-            <label>Number of Guests: {formData.groupSize}</label>
-                <input type="range" min='1' max='8' defaultValue={requestObject.group_size} name="groupSize" onChange={handleChange} required />
-            <br></br>
-            <label>Arrival Date:</label>
-                <input type="date" defaultValue={formData.arrivalDate} name="arrivalDate" onChange={handleChange} required />
-            <br></br>
-            <label>Departure Date:</label>
-                <input type="date" defaultValue={formData.departureDate} name="departureDate" onChange={handleChange} required />
-            <br></br>
-            <label>Request Message:</label>
-                <textarea name="requestMessage" defaultValue={formData.requestMessage} onChange={handleChange} required></textarea>
-            <label>Price:</label>
-                <input type="number" defaultValue={formData.price} name="price" min="0.00" max="10000.00"  step="50" onChange={handleChange}/>
-            <br></br>
-            <label>Payment Received?:</label>
-                <input type="radio" id="True" name="paymentReceived" value={true} onChange={handleChange}/><label>Yes</label>
-                <input type="radio" id="False" name="paymentReceived" value={false} checked onChange={handleChange}/><label>No</label>
-            <br></br>
-            <label>Admin Note:</label>
-                <textarea name="adminNote" defaultValue={formData.adminNote} onChange={handleChange}></textarea>
-            <button className="button" type="submit">Update Reservation</button>
-         </form>
-     </div>
+        <div className="max-w-xl mx-auto mt-11 p-4 bg-white shadow-md rounded-md">
+            <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Editing Booking ID: {requestObject.id}</label>
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Number of Guests: {formData.groupSize}</label>
+                    <input type="range" min="1" max="8" defaultValue={requestObject.group_size} name="groupSize" onChange={handleChange} required className="w-full" />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Arrival Date:</label>
+                    <input type="date" defaultValue={formData.arrivalDate} name="arrivalDate" onChange={handleChange} required className="w-full px-3 py-2 border rounded-md" />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Departure Date:</label>
+                    <input type="date" defaultValue={formData.departureDate} name="departureDate" onChange={handleChange} required className="w-full px-3 py-2 border rounded-md" />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Request Message:</label>
+                    <textarea name="requestMessage" defaultValue={formData.requestMessage} onChange={handleChange} required className="w-full px-3 py-2 border rounded-md"></textarea>
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Price:</label>
+                    <input type="number" defaultValue={formData.price} name="price" min="0.00" max="10000.00" step="50" onChange={handleChange} className="w-full px-3 py-2 border rounded-md" />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Payment Received?:</label>
+                    <div className="flex items-center">
+                        <input type="radio" id="True" name="paymentReceived" value={true} onChange={handleChange} className="mr-2" /><label className="mr-4">Yes</label>
+                        <input type="radio" id="False" name="paymentReceived" value={false} checked onChange={handleChange} className="mr-2" /><label>No</label>
+                    </div>
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Admin Note:</label>
+                    <textarea name="adminNote" defaultValue={formData.adminNote} onChange={handleChange} className="w-full px-3 py-2 border rounded-md"></textarea>
+                </div>
+                <button 
+                    type="submit" 
+                    className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+                >
+                    Update Reservation
+                </button>
+                <button 
+                    onClick={() => closeModal()}
+                    className="bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600 float-end"
+                >
+                    Cancel
+                </button>
+            </form>
+        </div>
     );
 };
 
