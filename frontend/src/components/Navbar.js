b85cc4dd494d9f09533e6da7edec9f03ee7b0016
Phil Michaels <93264586+PJMichaels@users.noreply.github.com>
2024-06-20 02:29:07 -0400

    bootstrap overhaul and styling

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,97 +1,153 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useAuth } from '../context/AuthContext';
 
+function Navbar() {
+
+    const { isLoggedIn, isLoading, logout, userGroups } = useAuth();
+    const isAdmin = isLoggedIn && userGroups.includes('Admin');
+    const isGuest = isLoggedIn && userGroups.includes('Guest');
+    const isLimitedGuest = isLoggedIn && userGroups.includes('LimitedGuest');
+
+    const handleSelect = (eventKey) => {
+        if (eventKey === 'logout') {
+            logout();
+        } 
+      };
 
 
-const Navbar = () => {
-
-  // Need to include isLoading to delay load..
-  const { isLoggedIn, isLoading, logout, userGroups } = useAuth();
-
-  const isAdmin = isLoggedIn && userGroups.includes('Admin');
-  const isGuest = isLoggedIn && userGroups.includes('Guest');
-  const isLimitedGuest = isLoggedIn && userGroups.includes('LimitedGuest');
-  // const isAdminOrGuest = isLoggedIn && (userGroups.includes('Admin') || userGroups.includes('Guest'));
-
-
-  const AdminRoutes = () => {
-    return (
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/date-provisioning">Date Provisioning</Link></li>
-      <li><Link to="/reservation-management">Reservation Management</Link></li>
-      <li><Link to="/user-management">User Management</Link></li>
-      <li><Link to="/booking">Book Dates</Link></li>
-      <li><Link to="/saco-river">Saco River Tubing</Link></li>
-      <li><Link to="/reservations">Reservations</Link></li>
-      <li><Link to="/stayinfo">Stay Info</Link></li>
-      <li><Link to="/checkout">Checkout Info</Link></li>
-      <li><Link to="/profile">My Profile</Link></li>
-      <li><button onClick={logout}>Logout</button></li>
-    </ul>
+    const AdminRoutes = () => (
+        <Nav variant="pills" onSelect={handleSelect}>
+            <Nav.Item>
+                <Nav.Link eventKey="home" title="Home" href="/">
+                Home
+                </Nav.Link>
+            </Nav.Item>
+            <NavDropdown title="Admin" id="nav-dropdown">
+                <NavDropdown.Item eventKey="date-provisioning" href='/date-provisioning'>Provision Dates</NavDropdown.Item>
+                <NavDropdown.Item eventKey="reservation-management" href='/reservation-management'>Manange Bookings</NavDropdown.Item>
+                <NavDropdown.Item eventKey="user-management" href='/user-management'>Manage Users</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Item>
+                <Nav.Link eventKey="gallery" title="Gallery" href="/gallery">
+                    Photo Gallery
+                </Nav.Link>
+            </Nav.Item>
+            <NavDropdown title="Activities" id="nav-dropdown">
+                <NavDropdown.Item eventKey="saco-river" href='/saco-river'>River Tubing</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Item>
+                <Nav.Link eventKey="booking" title="Book Dates" href="/booking">
+                Book Dates
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="profile" title="Profile" href="/profile">
+                Profile
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="logout">Logout</Nav.Link>
+            </Nav.Item>
+        </Nav>
+      );
+    
+    const GuestRoutes = () => (
+    <Nav variant="pills" onSelect={handleSelect}>
+        <Nav.Item>
+            <Nav.Link eventKey="home" title="Home" href="/">
+                Home
+            </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link eventKey="gallery" title="Gallery" href="/gallery">
+                Photo Gallery
+            </Nav.Link>
+        </Nav.Item>
+        <NavDropdown title="Activities" id="nav-dropdown">
+            <NavDropdown.Item eventKey="saco-river" href='/saco-river'>River Tubing</NavDropdown.Item>
+        </NavDropdown>
+        <Nav.Item>
+            <Nav.Link eventKey="booking" title="Book Dates" href="/booking">
+                Book Dates
+            </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link eventKey="profile" title="Profile" href="/profile">
+                Profile
+            </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link eventKey="logout">Logout</Nav.Link>
+        </Nav.Item>
+    </Nav>
     );
-  }
 
-  const GuestRoutes = () => {
-    return (
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/saco-river">Saco River Tubing</Link></li>
-      <li><Link to="/booking">Book Dates</Link></li>
-      <li><Link to="/reservations">Reservations</Link></li>
-      <li><Link to="/stayinfo">Stay Info</Link></li>
-      <li><Link to="/checkout">Checkout Info</Link></li>
-      <li><Link to="/profile">My Profile</Link></li>
-      <li><button onClick={logout}>Logout</button></li>
-    </ul>
-    );
-  }
+      const LimitedGuestRoutes = () => (
+        <Nav variant="pills" onSelect={handleSelect}>
+            <Nav.Item>
+                <Nav.Link eventKey="home" title="Home" href="/">
+                    Home
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="gallery" title="Gallery" href="/gallery">
+                    Photo Gallery
+                </Nav.Link>
+            </Nav.Item>
+            <NavDropdown title="Activities" id="nav-dropdown">
+                <NavDropdown.Item eventKey="saco-river" href='/saco-river'>River Tubing</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Item>
+                <Nav.Link eventKey="reservations" title="Reservations" href="/reservations">
+                    Reservations
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="profile" title="Profile" href="/profile">
+                    Profile
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="logout">Logout</Nav.Link>
+            </Nav.Item>
+        </Nav>
+      );
 
-  const LimitedGuestRoutes = () => {
-    return (
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/saco-river">Saco River Tubing</Link></li>
-      <li><Link to="/reservations">Reservations</Link></li>
-      <li><Link to="/stayinfo">Stay Info</Link></li>
-      <li><Link to="/checkout">Checkout Info</Link></li>
-      <li><Link to="/profile">My Profile</Link></li>
-      <li><button onClick={logout}>Logout</button></li>
-    </ul>
-    );
-  }
+      const PublicRoutes = () => (
+        <Nav variant="pills">
+            <Nav.Item>
+                <Nav.Link eventKey="home" title="Home" href="/">
+                    Home
+                </Nav.Link>
+            </Nav.Item>
+            <NavDropdown title="Activities" id="nav-dropdown">
+                <NavDropdown.Item eventKey="saco-river" href='/saco-river'>River Tubing</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Item>
+                <Nav.Link eventKey="login" title="Login" href="/login">
+                    Login
+                </Nav.Link>
+            </Nav.Item>
+        </Nav>
+      );
 
-  const PublicRoutes = () => {
-    return (
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/gallery">Gallery</Link></li>
-      <li><Link to="/saco-river">Saco River Tubing</Link></li>
-      <li><Link to="/login">Login</Link></li>
-    </ul>
-    );
-  }
-
-  const LoadingIndicator = () => (
-    <ul>
-      <li>Loading...</li>
-    </ul>
-  );
-  
+    const LoadingIndicator = () => (
+        <ul className="list-none m-0 p-0 overflow-hidden flex">
+          <li className="flex-shrink-0">Loading...</li>
+        </ul>
+      );
 
   return (
-    <nav>
-      {
-      isLoading ? <LoadingIndicator />:
-      isAdmin ? <AdminRoutes /> :
-      isGuest ? <GuestRoutes />:
-      isLimitedGuest ? <LimitedGuestRoutes />:
-      <PublicRoutes />
-
+    <nav className="bg-black text-white bg-opacity-80 p-2">
+      {isLoading ? <LoadingIndicator /> :
+        isAdmin ? <AdminRoutes /> :
+        isGuest ? <GuestRoutes /> :
+        isLimitedGuest ? <LimitedGuestRoutes /> :
+        <PublicRoutes />
       }
     </nav>
   );
-};
+}
 
 export default Navbar;
