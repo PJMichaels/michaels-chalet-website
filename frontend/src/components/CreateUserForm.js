1b2eb6708852c5989290321fce7a275004ed1fb8
Phil Michaels <93264586+PJMichaels@users.noreply.github.com>
2024-06-20 02:28:36 -0400

    new form

--- frontend/src/components/CreateUserForm.js
+++ frontend/src/components/CreateUserForm.js
@@ -45,33 +45,117 @@ const CreateUserForm = ({closeModal}) => {
     };
 
     return (
-        <div>
-         <form onSubmit={handleSubmit}>
-            <label>Create New User:</label>
-            <br></br>
-            <label>Name:</label>
-            <input type="text" defaultValue={formData.name} name="name" onChange={handleChange} required />
-            <br></br>
-            <label>Email:</label>
-                <input type="email" defaultValue={formData.email} name="email" onChange={handleChange} required />
-            <br></br>
-            <label>Phone Number:</label>
-                <input type="text" defaultValue={formData.phone} name="phone" onChange={handleChange} required />
-            <br></br>
-            <label>Role:</label>
-                <input type="radio" id="Admin" name="group" value={"Admin"} onChange={handleChange}/><label>Admin</label>
-                <input type="radio" id="Guest" name="group" value={"Guest"} checked onChange={handleChange}/><label>Guest</label>
-                <input type="radio" id="LimitedGuest" name="group" value={"LimitedGuest"} onChange={handleChange}/><label>LimitedGuest</label>
-            <br></br>
-            <label>Password:</label>
-            <input type="password" name="password" defaultValue={formData.password} onChange={handleChange}/>
-            <br/>
-            <label>Re-Enter Password:</label>
-            <input type="password" name="checkPassword" defaultValue={formData.checkPassword} onChange={handleChange}/>
-            <br/>
-            <button className="button" type="submit">Create User</button>
-         </form>
-     </div>
+        <div className="max-w-xl mx-auto mt-11 p-4 bg-white shadow-md rounded-md">
+            <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-lg font-bold mb-2">Create New User:</label>
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Name:</label>
+                    <input 
+                        type="text" 
+                        defaultValue={formData.name} 
+                        name="name" 
+                        onChange={handleChange} 
+                        required 
+                        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Email:</label>
+                    <input 
+                        type="email" 
+                        defaultValue={formData.email} 
+                        name="email" 
+                        onChange={handleChange} 
+                        required 
+                        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Phone Number:</label>
+                    <input 
+                        type="text" 
+                        defaultValue={formData.phone} 
+                        name="phone" 
+                        onChange={handleChange} 
+                        required 
+                        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Role:</label>
+                    <div className="flex items-center space-x-4">
+                        <label className="flex items-center">
+                            <input 
+                                type="radio" 
+                                id="Admin" 
+                                name="group" 
+                                value="Admin" 
+                                onChange={handleChange} 
+                                className="form-radio text-blue-500" 
+                            />
+                            <span className="ml-2 text-gray-700">Admin</span>
+                        </label>
+                        <label className="flex items-center">
+                            <input 
+                                type="radio" 
+                                id="Guest" 
+                                name="group" 
+                                value="Guest" 
+                                onChange={handleChange} 
+                                className="form-radio text-blue-500" 
+                                checked
+                            />
+                            <span className="ml-2 text-gray-700">Guest</span>
+                        </label>
+                        <label className="flex items-center">
+                            <input 
+                                type="radio" 
+                                id="LimitedGuest" 
+                                name="group" 
+                                value="LimitedGuest" 
+                                onChange={handleChange} 
+                                className="form-radio text-blue-500" 
+                            />
+                            <span className="ml-2 text-gray-700">Limited Guest</span>
+                        </label>
+                    </div>
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Password:</label>
+                    <input 
+                        type="password" 
+                        name="password" 
+                        defaultValue={formData.password} 
+                        onChange={handleChange} 
+                        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Re-Enter Password:</label>
+                    <input 
+                        type="password" 
+                        name="checkPassword" 
+                        defaultValue={formData.checkPassword} 
+                        onChange={handleChange} 
+                        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                    />
+                </div>
+                <button 
+                    type="submit" 
+                    className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300 m-1"
+                >
+                    Create User
+                </button>
+                <button 
+                    onClick={() => closeModal()}
+                    className="w-full bg-gray-500 text-white py-2 rounded-lg hover:bg-gray-600 transition duration-300 m-1"
+                >
+                    Cancel
+                </button>
+            </form>
+        </div>
     );
 };
 
